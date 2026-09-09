6f3895ed8d1e0f1d5fad11b48b3f159f7de1cc15
rattus <46076784+rattus128@users.noreply.github.com>
2026-09-10 01:51:34 +1000

    main: bump the AMD Windows VA quota to 4TB (#16199)
    AMD+windows sets an unusually low maximum GPU virtual memory quota
    compared to other plaforms. For reference, the hardware limits on
    XT9060 are 128TB. On Nvidia RTX3060 it is 1TB. On nvidia RTX5090
    it is 128TB.
    
    Bump this into the same league, but keep it to a modest 4TB pending
    further usage case need + testing going even higher to the hardware
    limits.

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ if __name__ == "__main__":
             os.environ['CUBLAS_WORKSPACE_CONFIG'] = ":4096:8"
 
     if "rocm" in cuda_malloc.get_torch_version_noimport():
-        os.environ['OCL_SET_SVM_SIZE'] = '262144'  # set at the request of AMD
+        os.environ['OCL_SET_SVM_SIZE'] = '4194304'  # 4TB. Much larger than the ROCM 64GB/256GB defaults for Aimdos liberal VA use
 
 
 def handle_comfyui_manager_unavailable():

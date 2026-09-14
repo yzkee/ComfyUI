50ab50c15a313d5ec6436b6cdc63fc3fca91da0a
rattus <46076784+rattus128@users.noreply.github.com>
2026-09-15 06:56:34 +1000

    Implement Generic Loops (Candidate III - implemented ✅) (CORE-14) (#16227)

--- nodes.py
+++ nodes.py
@@ -2539,6 +2539,7 @@ async def init_builtin_extra_nodes():
         "nodes_depth_anything_3.py",
         "nodes_seed.py",
         "nodes_text.py",
+        "nodes_loop.py",
         "nodes_sam3d_body.py",
         "nodes_marigold.py",
     ]
